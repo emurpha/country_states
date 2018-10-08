f4687da53e5eae65f968b7b6a32cab9fc08a2e10
Emily  Murphy <Emily.Murphy@emurphy.2ldxhoqz4w0efi5ajh3u0k0m2g.bx.internal.cloudapp.net>
2018-10-08 20:07:08 +0000

    Add country input boxes

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../country';
 
 @Component({
   selector: 'app-countries',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countries.component.css']
 })
 export class CountriesComponent implements OnInit {
+  country: Country = {
+    id: 1,
+    code: 'Af',
+    name: 'Afghanistan'
+  }
 
   constructor() { }
 
