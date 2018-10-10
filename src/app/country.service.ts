27c77e725d981c0a5b31c4f6378e3caa05d5027e
Emily  Murphy <Emily.Murphy@emurphy.2ldxhoqz4w0efi5ajh3u0k0m2g.bx.internal.cloudapp.net>
2018-10-10 19:12:48 +0000

    WIP: HTTP Get Request

--- src/app/country.service.ts
+++ src/app/country.service.ts
@@ -5,18 +5,29 @@ import { COUNTRIES } from './mock-countries';
 
 import { Observable, of } from 'rxjs';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
 
   constructor(
+    private http: HttpClient
   ){}
 
   private countriesUrl = 'api/countries'; //URL to Web api
 
   getCountries(): Observable<Country[]> {
-    return of(COUNTRIES);
+    return this.http.get<Country[]>(this.countriesUrl)
+  }
+
+  addCountry(country: Country): Observable<Country> {
+    return this.http.post<Country>(this.countriesUrl, country, httpOptions)
   }
   
 
